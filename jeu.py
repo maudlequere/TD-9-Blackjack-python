14d54f0d083d50d69bca36a5aaf94e2a9578228b
Maud <maud.fandresena@gmail.com>
2025-11-25 12:10:56 +0100

    version après pause premier jour

--- jeu.py
+++ jeu.py
@@ -51,8 +51,8 @@ def get_value_cards(liste_cartes):
     if len(lst_ace)!=0:
         while e < len(lst_ace) :
             e = e + 1 
-            if somme <=11 :
-                somme = somme + 10
+            if somme <=10 :
+                somme = somme + 11
             else :
                 somme = somme + 1
             
@@ -64,7 +64,80 @@ print(get_value_cards(['2 ♣','2 ♠','3 ♥','As ♥']))
 print(get_value_cards(['As ♥','Valet ♥','Valet ♦']))
 print(get_value_cards(['Valet ♥','Valet ♦','As ♥']))
 
-#fonction display => cartes ainsi que la valeur cumulée
+#fonction display_card => cartes ainsi que la valeur cumulée
 
-def display(statut,lst_cards_player,value_player):
+def display_card(statut,lst_cards_player,value_player):
+    espace =" "
+    return f"{statut} {espace.join(lst_cards_player)} ({get_value_cards(value_player)})"
+
+print(display_card("Joueur",['2 ♣','2 ♠','3 ♥','As ♥'],['2 ♣','2 ♠','3 ♥','As ♥']))
+
+#fonction to_bet => nombre de dollars restant maj en fonction de la mise et la mise
+
+def to_bet(dollars_restant):
+
+    mise = 0
+    while mise > dollars_restant or mise <20:
+        while True :
+            try :
+                mise = int(input("Entrez la valeur que vous souhaitez miser :"))
+                break
+            except ValueError:
+                print("Oups la valeur n'est pas valide, entrez à nouveau une mise (en chiffres) :")
+        
+    dollars_restant = dollars_restant - mise
     
+    return [dollars_restant,mise]
+
+print(to_bet(50))
+
+#Jeu du blackjack
+dollars_restant = 200
+croupier = "Croupier"
+joueur = "Joueur"
+jouer = input("Voulez-vous jouer ?Répondre par oui ou non")
+
+while dollars_restant >= 20 or jouer.str.lower()!="oui" :
+    jeu = init_cards()
+
+    mise = to_bet(dollars_restant)
+
+    lst_card_dealer = []
+    lst_card_dealer.append(draw(jeu)[0])
+    value_dealer = get_value_cards(lst_card_dealer)
+    display_card(croupier,lst_card_dealer,value_dealer)
+
+    piocher = input("Voulez vous piocher une autre carte ? Répondre par oui ou non")
+    while piocher.str.lower() == "oui" :
+        lst_card_player = []
+        lst_card_player.append(draw(jeu)[0])
+        value_player = get_value_cards(lst_card_player)
+        if value_player > 21:
+            "Vous avez perdu"
+            winner_player = False
+            break
+
+    print(display_card(croupier,lst_card_dealer,value_dealer))
+    print(display_card(joueur,lst_card_player,value_player))
+
+    while value_dealer<value_player :
+        lst_card_dealer = []
+        lst_card_dealer.append(draw(jeu)[0])
+        value_dealer = get_value_cards(lst_card_dealer)
+        if value_dealer > 21:
+            "Le dealer a perdu"
+            winner_player = True
+            break
+    
+    if value_dealer > value_player and winner_player != True:
+        dollars_restant = dollars_restant - mise
+    else :
+        dollars_restant = dollars_restant + mise*2
+    
+    
+
+
+
+
+
+
